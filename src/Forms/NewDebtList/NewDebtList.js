e45f2859a11ef26303f42577fba981542364e24f
Linden Daniels <lindendaniels@gmail.com>
2019-12-27 18:57:03 -0500

    Refactor history url

--- src/Forms/NewDebtList/NewDebtList.js
+++ src/Forms/NewDebtList/NewDebtList.js
@@ -141,14 +141,16 @@ updateFormEntry(e) {
         const STORE = this.props.store;
         const folderId = this.state.folderId
        
-        const folder = this.props.store.folders.find(folder => +folder.id === +folderId)        
+        const folder = this.props.store.folders.find(folder => +folder.id === +folderId)
+        const folderUrl = `/folder/${forlderId}`        
 
 
         //const debtFolder = this.props.store.debtFolder
         
         
         folder.debts.push(newDebtList);
-        this.props.history.push('/folder-list/');
+        
+        this.props.history.push(folderUrl);
 
         this.setState({error: null})
     };
